826deb859c8b94f7dd23924881503c3edbc5760d
serenashen1 <102195030+serenashen1@users.noreply.github.com>
2023-11-06 00:59:48 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -2,12 +2,12 @@ var video;
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window");
-	video = document.querySelector('#player1');
-	video.autoplay = false;
-	video.loop = false;
-	console.log("Auto play is set to " + video.autoplay);
-	console.log("Loop is set to " + video.loop);
-});
+// 	video = document.querySelector('#player1');
+// 	video.autoplay = false;
+// 	video.loop = false;
+// 	console.log("Auto play is set to " + video.autoplay);
+// 	console.log("Loop is set to " + video.loop);
+// });
 
 // document.querySelector("#play").addEventListener("click", function() {
 // 	console.log("Play Video");
